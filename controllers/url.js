edea438f1dc4f6cdc7ecc019b7a003c9fe1d307f
Rushikesh Jawale <100342436+imrushikesh77@users.noreply.github.com>
2023-08-15 10:47:41 +0530

    Add files via upload

---
+++ controllers/url.js
@@ -0,0 +1,31 @@
+const shortid  = require("shortid");
+const URL = require("../models/url");
+
+async function handleGenerateNewURL(req,res){
+    const body = req.body;
+    // console.log(body);
+    if(!body.url) return res.status(400).json({error:"url is required"});
+    const shortID = shortid();
+    await URL.create({
+        shortId:shortID,
+        redirectURL:body.url,
+        visitHistory:[],
+    });
+    return res.render("home",{
+        id:shortID,
+    })
+}
+
+async function handleGetAnalytics(req,res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    })
+}
+
+module.exports = {
+    handleGenerateNewURL,
+    handleGetAnalytics,
+}
